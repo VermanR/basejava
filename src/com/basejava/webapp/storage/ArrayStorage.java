11720a07c960e9b85d2a560d0794ada63d0d92be
VermanR <ruslanverman@gmail.com>
2020-06-29 22:33:56 +0300

    HW2 new changes, added local variable

--- src/com/basejava/webapp/storage/ArrayStorage.java
+++ src/com/basejava/webapp/storage/ArrayStorage.java
@@ -15,46 +15,41 @@ public class ArrayStorage {
         Arrays.fill(storage, 0, size, null);
     }
 
-    public void update(Resume r) {                    //резюме есть в storage?
-        if (searchResume(r.getUuid()) == 0) {
-            System.out.println("ERROR: resume " + r.getUuid() + " not found, get command failed");
-            return;
+    public void update(Resume resume) {
+        int tmp = searchResume(resume.getUuid());            //резюме нет в storage?
+        if (tmp != -1) {
+            System.out.println("ERROR: resume " + resume.getUuid() + " not found, update command failed");
+        } else {
+            storage[size] = resume;
         }
-        searchResume(r.getUuid());
     }
 
-    public void save(Resume r) {                     //резюме нет в storage?
-        if (searchResume(r.getUuid()) != 0) {
-            System.out.println("ERROR: this resume is already there, save command failed");
-            return;
+    public void save(Resume resume) {
+        int tmp = searchResume(resume.getUuid());            //резюме нет в storage?
+        if (tmp != -1) {
+            System.out.println("ERROR: resume " + resume.getUuid() + " is already there, save command failed");
         } else if (size == storage.length) {              // проверка на переполнение
             System.out.println("ERROR: array is full");
         } else {
-            storage[size] = r;
+            storage[size] = resume;
             size++;
         }
     }
 
-    public Resume get(String uuid) {                 //резюме есть в storage?
-        if (searchResume(uuid) == 0) {
+    public Resume get(String uuid) {
+        int tmp = searchResume(uuid);                       //резюме есть в storage?
+        if (tmp == -1) {
             System.out.println("ERROR: resume " + uuid + " not found, get command failed");
             return null;
         }
-        return storage[searchResume(uuid)];
-        //searchResume(uuid);
+        return storage[tmp];
     }
 
     public void delete(String uuid) {                 //резюме есть в storage?
-        if (searchResume(uuid) == 0) {
+        if (searchResume(uuid) == -1) {
             System.out.println("ERROR: resume " + uuid + " not found, delete command failed");
-            return;
         } else {
-            for (int j = 0; j < size; j++) {
-                if (uuid.equals(storage[j].getUuid())) {
-                    if (size - 1 - j >= 0) System.arraycopy(storage, j + 1, storage, j, size - 1 - j);
-                    size--;
-                }
-            }
+            System.arraycopy(storage, 0, storage, 0, size--);
         }
     }
 
@@ -76,6 +71,6 @@ public class ArrayStorage {
                 return i;
             }
         }
-        return 0;
+        return -1;
     }
 }
