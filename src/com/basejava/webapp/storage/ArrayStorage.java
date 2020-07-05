2e48d0c90c3329796f01c9a3c6023dedc5d6c479
VermanR <ruslanverman@gmail.com>
2020-07-05 22:15:01 +0300

    HW3 first commit

--- src/com/basejava/webapp/storage/ArrayStorage.java
+++ src/com/basejava/webapp/storage/ArrayStorage.java
@@ -7,26 +7,28 @@ import java.util.Arrays;
 /**
  * Array based storage for Resumes
  */
-public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
-    private int size = 0;
+public class ArrayStorage extends AbstractArrayStorage{
+    /*public static final int STORAGE_LIMIT = 10000;
+    private Resume[] storage = new Resume[STORAGE_LIMIT];
+    private int size = 0;*/
 
     public void clear() {
         Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
     public void update(Resume resume) {
-        int index = searchResume(resume.getUuid());            //резюме нет в storage?
+        int index = searchResume(resume.getUuid());            //резюме есть в storage?
         if (index == -1) {
             System.out.println("ERROR: resume " + resume.getUuid() + " not found, update command failed");
         } else {
-            storage[size] = resume;
+            storage[index] = resume;
         }
     }
 
     public void save(Resume resume) {
         int index = searchResume(resume.getUuid());            //резюме нет в storage?
-        if (index == -1) {
+        if (index != -1) {
             System.out.println("ERROR: resume " + resume.getUuid() + " is already there, save command failed");
         } else if (size >= storage.length) {              // проверка на переполнение
             System.out.println("ERROR: array is full");
@@ -60,10 +62,6 @@ public class ArrayStorage {
      */
     public Resume[] getAll() {
         return Arrays.copyOf(storage, size);
-    }
-
-    public int size() {
-        return size;
     }
 
     private int searchResume(String uuid) {
